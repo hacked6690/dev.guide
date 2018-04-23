340f5b6308215c748eed167acfb6c5ecd5f5f3b4
hacked6690 <hacked6690@gmail.com>
2018-04-23 10:48:00 +0700

    ajx +v.01

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -59,7 +59,7 @@ class AjaxController extends Controller
 
     public function realtime_upload(Request $request)
     {
-        // assign empty array ;
+        // assign empty array +v.01;
         $msg = \Helper::layout()->label->realtime->title;
         $success = $errors = [];
 
