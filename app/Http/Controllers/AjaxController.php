d4e283d0710a48e2f7f9fb79c061700120815be4
hacked6690 <hacked6690@gmail.com>
2018-04-23 11:52:07 +0700

    delete sth

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -24,7 +24,6 @@ class AjaxController extends Controller
         $this->middleware('auth');
     }
 
-    // removed sth. ;
     // these are fixed method 0.1;
     public function of_taxonomy(Request $request)
     {
