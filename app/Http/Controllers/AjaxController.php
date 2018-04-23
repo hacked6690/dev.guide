b69aff6a28f6581451e55e0a5cd65f075bc21ec5
hacked6690 <hacked6690@gmail.com>
2018-04-23 10:38:21 +0700

    ajx +

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -24,7 +24,7 @@ class AjaxController extends Controller
         $this->middleware('auth');
     }
 
-    // Fixed method - ;
+    // these are fixed method ;
     public function of_taxonomy(Request $request)
     {
         $result = false;
