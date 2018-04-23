4dcdd52f5eef0b4b0ed6ef04cfa503f064d6c1ae
VannochGIGB <38314857+VannochGIGB@users.noreply.github.com>
2018-04-23 10:43:55 +0700

    o-

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -24,7 +24,7 @@ class AjaxController extends Controller
         $this->middleware('auth');
     }
 
-    // these are fixed method ;
+    // these are fixed method 0.1;
     public function of_taxonomy(Request $request)
     {
         $result = false;
